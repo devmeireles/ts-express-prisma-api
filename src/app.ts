ab145e778bfe6b037116ee86dfb80fdc544fb6e0
Gabriel Santos Meireles <dev.meireles@gmail.com>
2022-08-28 19:45:55 -0300

    Adding tests

--- src/app.ts
+++ src/app.ts
@@ -4,45 +4,11 @@ import cors from "cors";
 import * as dotenv from "dotenv";
 import { PrismaClient } from "@prisma/client";
 import * as swagger from "express-jsdoc-swagger";
+
 import { userRouter } from "./routes/user.route";
+import { swaggerOptions } from "@config/swagger";
 
 dotenv.config();
-
-const options = {
-  info: {
-    version: "1.0.0",
-    title: "Rent a House",
-    license: {
-      name: "MIT",
-    },
-  },
-  security: {
-    BasicAuth: {
-      type: "http",
-      scheme: "basic",
-    },
-  },
-  // Base directory which we use to locate your JSDOC files
-  baseDir: __dirname,
-  // Glob pattern to find your jsdoc files (multiple patterns can be added in an array)
-  filesPattern: "./**/*.ts",
-  // URL where SwaggerUI will be rendered
-  swaggerUIPath: "/docs",
-  // Expose OpenAPI UI
-  exposeSwaggerUI: true,
-  // Expose Open API JSON Docs documentation in `apiDocsPath` path.
-  exposeApiDocs: false,
-  // Open API JSON Docs endpoint.
-  apiDocsPath: "/v3/docs",
-  // Set non-required fields as nullable by default
-  notRequiredAsNullable: false,
-  // You can customize your UI options.
-  // you can extend swagger-ui-express config. You can checkout an example of this
-  // in the `example/configuration/swaggerOptions.js`
-  swaggerUiOptions: {},
-  // multiple option in case you want more that one instance
-  multiple: true,
-};
 
 class App {
   public express: express.Application;
@@ -53,7 +19,7 @@ class App {
     this.connection = new PrismaClient();
     this.middlewares();
     this.routes();
-    swagger.default(this.express)(options);
+    swagger.default(this.express)(swaggerOptions);
   }
 
   private middlewares(): void {
---
+++ src/config/swagger.ts
@@ -0,0 +1,24 @@
+export const swaggerOptions = {
+    info: {
+      version: "1.0.0",
+      title: "Rent a House",
+      license: {
+        name: "MIT",
+      },
+    },
+    security: {
+      BasicAuth: {
+        type: "http",
+        scheme: "basic",
+      },
+    },
+    baseDir: __dirname,
+    filesPattern: "./**/*.ts",
+    swaggerUIPath: "/docs",
+    exposeSwaggerUI: true,
+    exposeApiDocs: false,
+    apiDocsPath: "/v3/docs",
+    notRequiredAsNullable: false,
+    swaggerUiOptions: {},
+    multiple: true,
+  };
